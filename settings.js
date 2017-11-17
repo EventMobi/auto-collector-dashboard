22530528df58e26d8a358da87f07f056ee567e05
Viacheslav Liakhov <viacheslav@eventmobi.com>
2017-11-17 15:29:22 -0500

    Actual projectId and readKey

--- settings.js
+++ settings.js
@@ -7,7 +7,7 @@ window.settings = function() {
   // This is the projectId and readKey for an example project
   // Replace both with your own projectId and readKey to visualize data from your own Auto-Collector project  
   window.renderCharts(new Keen({
-    projectId: '5812b6058db53dfda8a75af9',
-    readKey: '9AD57194D00E31D4D7D0DF02EF72E00162C6E515B343BB3BB75EFFD0B00D8DF1'
+    projectId: '53bd52c7c9e16314e0000001',
+    readKey: 'ee61dc72de2d80458dab0f1b1c743b566e9dfc09a20880c4fb2319f11701f776b30bf84ef56ed1f6afa35c6928a9ae3b7aab8ba40f2601125d184ce3a8cb390606eb982f9e53bcfed997c49be9e2b43a65ecd6cf2104dfa5842d6aaee0a0c2a66276a968dca48019691cd231930f9209'
   }), timeframe);
 }
